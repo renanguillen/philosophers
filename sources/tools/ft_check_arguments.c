18f76a6eab3ee61a1423bf44a71db9bededd18bb
renanguillen <rigcpj@gmail.com>
2023-05-03 17:19:06 -0300

    argument changes

--- sources/tools/ft_check_arguments.c
+++ sources/tools/ft_check_arguments.c
@@ -6,7 +6,7 @@
 /*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 16:29:02 by ridalgo-          #+#    #+#             */
-/*   Updated: 2023/05/03 17:06:18 by ridalgo-         ###   ########.fr       */
+/*   Updated: 2023/05/03 17:18:29 by ridalgo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,13 @@ int	ft_check_arguments(int argc, char **argv)
 		exit_code = ERROR_ARGUMENT;
 	else if (ft_atoi(argv[1]) < 2)
 		exit_code = ERROR_ARGUMENT;
-	else if (ft_atoi(argv[2]) < 0)
+	else if (ft_atoi(argv[2]) <= 0)
 		exit_code = ERROR_ARGUMENT;
-	else if (ft_atoi(argv[3]) < 0)
+	else if (ft_atoi(argv[3]) <= 0)
 		exit_code = ERROR_ARGUMENT;
-	else if (ft_atoi(argv[4]) < 0)
+	else if (ft_atoi(argv[4]) <= 0)
 		exit_code = ERROR_ARGUMENT;
-	else if (argc == 6 && ft_atoi(argv[5]) < 0)
+	else if (argc == 6 && ft_atoi(argv[5]) < 1)
 		exit_code = ERROR_ARGUMENT;
 	return (exit_code);
 }
