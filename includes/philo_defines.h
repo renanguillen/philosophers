49d899b257034c43c10d57fe3357a7b1ff021b73
renanguillen <rigcpj@gmail.com>
2023-05-10 17:11:23 -0300

    thread introduction

--- includes/philo_defines.h
+++ includes/philo_defines.h
@@ -6,7 +6,7 @@
 /*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 17:12:32 by ridalgo-          #+#    #+#             */
-/*   Updated: 2023/05/10 16:14:21 by ridalgo-         ###   ########.fr       */
+/*   Updated: 2023/05/10 16:37:44 by ridalgo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,13 @@
 
 // Argument indexes
 # define CHECK_OK 0
-# define ERR_ARG_COUNT 1
-# define ERR_PHILO_COUNT 2
-# define ERR_TTD 3
-# define ERR_TTE 4
-# define ERR_TTS 5
-# define ERR_EAT_COUNT 6
+# define ERR_ARG_COUNT -1
+# define ERR_PHILO_COUNT -2
+# define ERR_TTD -3
+# define ERR_TTE -4
+# define ERR_TTS -5
+# define ERR_EAT_COUNT -6
 
+// Exit codes
+# define ERR_MEMORY 1
 #endif
